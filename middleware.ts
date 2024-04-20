35d027dbc5c6281388d327d3ca565b9a9951a8ae
taishikato <taishi.k0903@gmail.com>
2024-04-20 16:07:23 +0700

    Refactor middleware for creating Supabase client

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,42 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "./utils/supabase/createMiddlewareSupabaseClient";
+import { NextResponse, type NextRequest } from "next/server";
+import { createClient } from "./utils/supabase/createMiddlewareSupabaseClient";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  try {
+    const { supabase, response } = createClient(request);
+
+    // Refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+    await supabase.auth.getUser();
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    // For protected API handlers
+    if (request.nextUrl.pathname.startsWith("/api/protected")) {
+      // Check auth condition
+      if (!session) {
+        // No session. Return 401 error.
+        return new Response(
+          JSON.stringify({ result: "error", message: "Please log in." }),
+          {
+            status: 401,
+          }
+        );
+      }
+    }
+
+    return response;
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    // This is likely because you have not set up environment variables.
+    // Check out http://localhost:3000 for Next Steps.
+    return NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+  } // For protected API handlers
 }
 
 export const config = {
