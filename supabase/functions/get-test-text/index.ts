c2213fb48dc2e63c2db9ebb8444eff7ca34e9589
taishikato <taishi.k0903@gmail.com>
2024-04-21 11:05:38 +0700

    dont wait the promise done and just return the response first

--- supabase/functions/get-test-text/index.ts
+++ supabase/functions/get-test-text/index.ts
@@ -30,10 +30,12 @@ Deno.serve(async (req) => {
   const patternToReturn = data[randomChoice()]
 
   // count up show count for this pattern
-  const { data: d, error: e } = await supabaseAdmin.rpc("increment_pattern_show_count", {
+  supabaseAdmin.rpc("increment_pattern_show_count", {
     x: 1,
     patternid: patternToReturn.id
-  });
+  }).then(res => {
+    console.log(res)
+  })
 
   return new Response(
     JSON.stringify(patternToReturn),
@@ -41,14 +43,3 @@ Deno.serve(async (req) => {
   )
 })
 
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/get-test-text' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
