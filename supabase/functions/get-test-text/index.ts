ef7f47cd396bee40951d37be53b9f03e65113231
taishikato <taishi.k0903@gmail.com>
2024-04-21 14:33:36 +0700

    Refactor get-test-text function for error handling

--- supabase/functions/get-test-text/index.ts
+++ supabase/functions/get-test-text/index.ts
@@ -5,41 +5,54 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 /// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
 
-console.log("Hello from Functions!")
-
 const randomChoice = () => Math.random() < 0.5 ? 0 : 1;
 
 type Data = {
-    id: string
-    text: string
-    test_id: string
-  }
+  id: string;
+  text: string;
+  test_id: string;
+};
 
-import { supabaseAdmin } from "../_shared/supabaseAdmin.ts"
+import { supabaseAdmin } from "../_shared/supabaseAdmin.ts";
 
 Deno.serve(async (req) => {
-  const { testId } = await req.json()
+  try {
+    const { testId } = await req.json();
 
-  // get patterns for the test
-  const { data } = await supabaseAdmin.from("patterns").select("id, text").match({
-    test_id: testId,
-    is_active: true
-  })
+    // get patterns for the test
+    const { data } = await supabaseAdmin.from("patterns").select("id, text")
+      .match({
+        test_id: testId,
+        is_active: true,
+      });
 
-  // pick one
-  const patternToReturn = data[randomChoice()]
+    if (!data || data.length === 0) {
+      throw new Error("There is no active patters for this Test");
+    }
 
-  // count up show count for this pattern
-  supabaseAdmin.rpc("increment_pattern_show_count", {
-    x: 1,
-    patternid: patternToReturn.id
-  }).then(res => {
-    console.log(res)
-  })
+    // pick one
+    const patternToReturn = data[randomChoice()];
 
-  return new Response(
-    JSON.stringify(patternToReturn),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+    // count up show count for this pattern
+    supabaseAdmin.rpc("increment_pattern_show_count", {
+      x: 1,
+      patternid: patternToReturn.id,
+    }).then((res) => {
+      console.log(res);
+    });
 
+    return new Response(
+      JSON.stringify(patternToReturn),
+      { headers: { "Content-Type": "application/json" } },
+    );
+  } catch (err) {
+    const errorMessage = (err as Error).message;
+
+    console.error(errorMessage);
+
+    return new Response(
+      JSON.stringify({ status: "error", message: errorMessage }),
+      { status: 500, headers: { "Content-Type": "application/json" } },
+    );
+  }
+});
